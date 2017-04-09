bfa8587c776f0b59a249b7696181b19eb3dd04e7
rickyx12 <aizekrysler@gmail.com>
2017-04-09 22:58:46 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Route::get('book','BookController@index')->name('homeBook');
 Route::post('book/add','BookController@store')->name('addBook');
 Route::post('book/update/{id}','BookController@update')->name('updateStatus');
+Route::post('book/delete/{id}','BookController@destroy')->name('delete');
 Route::get('book/search','BookController@search')->name('searchText');
 Route::get('book/filter','BookController@filter')->name('filter');
 
